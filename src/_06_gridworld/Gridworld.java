69c5cccb8a7f45b0c6fa360a297e50c130056e0d
root <root@a2e6c45a8949>
2020-09-25 23:39:51 +0000

    Gridworld, Constructors

--- src/_06_gridworld/Gridworld.java
+++ src/_06_gridworld/Gridworld.java
@@ -3,6 +3,7 @@ package _06_gridworld;
 import java.awt.Color;
 
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
@@ -18,5 +19,17 @@ public class Gridworld {
 		world.add(loc2, bug2);
 		bug2.turn();
 		bug2.turn();
+		Flower flower1 = new Flower();
+		Flower flower2 = new Flower();
+		Location loc3 = new Location(7,5);
+		Location loc4 = new Location(7,7);
+		world.add(loc3, flower1);
+		world.add(loc4, flower2);
+		for(int a = 0; a < 10; a++) {
+		for(int i = 0; i < 10; i++) {
+			Location loc5 = new Location(i,a);
+			world.add(loc5, flower1);
+		}
+		}
 	}
 }
--- src/_07_cow_timer/CowTimer.java
+++ src/_07_cow_timer/CowTimer.java
@@ -9,16 +9,19 @@ import java.io.IOException;
 import javax.swing.JApplet;
 
 public class CowTimer {
-
+	private int minutes;
 	/*
 	 * 1. Make a constructor for the CowTimer class that initializes the minutes
 	 * variable
-	 */
-
+	*/
+	public CowTimer() {
+		minutes = 1;
+	}
+	
 	/* 4. Complete the main method of the CowTimerRunner class */
 
-	private int minutes;
-
+	
+	
 	public void setTime(int minutes) {
 		this.minutes = minutes;
 		System.out.println("Cow time set to " + minutes + " minutes.");
@@ -29,7 +32,11 @@ public class CowTimer {
 		 * 2. Count down the minutes, print the current minute then sleep for the number
 		 * of minutes using Thread.sleep(int milliseconds).
 		 */
-
+		for(int i = minutes; i>=0; i--) {
+			System.out.println(i);
+			Thread.sleep(60000);
+		}
+		System.out.println("Moo");
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
 		 * You can use the .wav file in the default package, or you can download one
